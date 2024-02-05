38100429866f40fdaac5f59aef73cf91d78cb464
Rodrigo <ediondonfullimer@gmail.com>
2024-02-05 15:26:21 -0300

    Adicionados: Credenciais do banco de dados e string de conecção

--- 12_ReactGram/backend/config/db.js
+++ 12_ReactGram/backend/config/db.js
@@ -1,3 +1,22 @@
 const mongoose = require('mongoose');
 
 // Conexão com o banco de dados
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS
+
+const conn = async () => {
+    try {
+        const dbConn = await mongoose.connect(
+            `mongodb+srv://${dbUser}:${dbPassword}@cluster0.iictu9w.mongodb.net/?retryWrites=true&w=majority`
+        );
+        console.log("Conectado ao banco de dados...");
+
+        return dbConn;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+conn() // Chama a função de conexão com o banco de dados
+
+module.exports = conn // Exporta a conexão com o banco de dados para ser utilizada em outros arquivos
