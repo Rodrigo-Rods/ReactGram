6a76158377cee154dc6903f50e64dbfb30abcf7a
Rodrigo <ediondonfullimer@gmail.com>
2024-02-07 13:00:43 -0300

    Criando a validação de autenticação.

---
+++ 12_ReactGram/backend/middlewares/authGuard.js
@@ -0,0 +1,24 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken'); // Bibioteca com metodos
+const jwtSecret = process.env.JWT_PASS; // Valor salvo da app
+
+const authGuard = async (req, res, next) => {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1]; // O token vem assim: Ex: Bearer¢1293u083201j98 o split é o ¢.
+
+    // Checar se o token existe
+    if (!token) return res.status(401).json({ errors: ["Acesso negado pois o token não foi encontrado."] });
+
+    //Verificar a validade do token
+    try {
+        const verified = jwt.verify(token, jwtSecret);
+
+        req.user = await User.findById(verified.id).select("-password"); // .select("-password") -> Não retorna a senha do usuário na requisição.
+
+        next();
+    } catch (error) {
+        res.status(401).json({ errors: ["Token inválido ou expirado."] });
+    }
+};
+
+module.exports = authGuard;
