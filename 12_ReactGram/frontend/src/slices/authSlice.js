ac5fe9e7d023d99b506ff99ad1b7fe1a67856ae0
Rodrigo <ediondonfullimer@gmail.com>
2024-02-19 12:09:09 -0300

    Fix parsing error in authSlice.js and add authReducer to store.js

--- 12_ReactGram/frontend/src/slices/authSlice.js
+++ 12_ReactGram/frontend/src/slices/authSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import authService from '../services/authService';
 
-const user = JSON.parce(localStorage.getItem('user'));
+const user = JSON.parse(localStorage.getItem('user'));
 
 const initialState = {
     user: user ? user : null,
@@ -23,3 +23,33 @@ export const register = createAsyncThunk("auth/register",
         return data;
     }
 )
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        reset: (state) => {
+            state.loading = false;
+            state.error = false;
+            state.success = false;
+        }
+    },
+    extraReducers: (builder) => {
+        builder.addCase(register.pending, (state) => {
+            state.loading = true;
+            state.error = false;
+        }).addCase(register.fulfilled, (state, action) => {
+            state.loading = false;
+            state.sucess = true;
+            state.error = null;
+            state.user = action.payload;
+        }).addCase(register.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+            state.user = null;
+        })
+    },
+});
+
+export const { reset } = authSlice.actions;
+export default authSlice.reducer;
