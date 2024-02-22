6db0554f84bc125b4286a01899cab0354ae83993
Rodrigo <ediondonfullimer@gmail.com>
2024-02-22 13:38:02 -0300

    Reducer de usuário

--- 12_ReactGram/frontend/src/App.js
+++ 12_ReactGram/frontend/src/App.js
@@ -15,6 +15,7 @@ import Footer from './components/Footer';
 import Home from './pages/Home/Home';
 import Login from './pages/Auth/Login';
 import Register from './pages/Auth/Register';
+import EditProfile from './pages/EditProfile/EditProfile';
 
 function App() {
   const { auth, loading } = useAuth()
@@ -30,7 +31,12 @@ function App() {
         <Navbar />
         <div className="container">
           <Routes>
-            <Route path="/" element={auth ? <Home /> : < Navigate to="login" />} />
+            <Route
+              path="/"
+              element={auth ? <Home /> : < Navigate to="login" />} />
+            <Route
+              path="/profile"
+              element={auth ? <EditProfile /> : <Navigate to="/login" />} />
             <Route path="/login" element={!auth ? <Login /> : <Navigate to="/" />} />
             <Route path="/register" element={!auth ? <Register /> : <Navigate to="/" />} />
           </Routes>
---
+++ 12_ReactGram/frontend/src/slices/userSlice.js
@@ -0,0 +1,26 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const initialState = {
+    user: {},
+    error: false,
+    sucess: false,
+    loading: false,
+    message: null
+}
+
+//Funções
+
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        resetMessage: (state) => {
+            state.message = null;
+        },
+    },
+});
+
+
+export const { resetMessage } = userSlice.actions;
+export default userSlice.reducer;
