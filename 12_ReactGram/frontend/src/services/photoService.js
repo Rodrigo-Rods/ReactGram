537f21e0d10fdaf1eda94f3a8a07764d802d53f0
Rodrigo <ediondonfullimer@gmail.com>
2024-02-27 13:26:47 -0300

    Service de edição de fotos

--- 12_ReactGram/frontend/src/services/photoService.js
+++ 12_ReactGram/frontend/src/services/photoService.js
@@ -45,11 +45,28 @@ const deletePhoto = async (id, token) => {
 };
 
 
+// Update uma foto
+
+const updatePhoto = async (id, data, token) => {
+    const config = requestConfig("PUT", data, token)
+    try {
+        const res = await fetch(api + '/photos/' + id, config)
+            .then((res) => res.json())
+            .catch((err) => err);
+
+        return res;
+    } catch (error) {
+        console.log('Erro:', error)
+    }
+};
+
+
 //Exportar funções
 const photoService = {
     publishPhoto,
     getUserPhotos,
     deletePhoto,
+    updatePhoto,
 };
 
 export default photoService
