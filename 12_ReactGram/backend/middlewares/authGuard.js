4388f0b0dd48f3bab81f509ed3df8b23f0e3d16e
Rodrigo <ediondonfullimer@gmail.com>
2024-06-17 18:59:17 -0300

    .gitignore

--- 12_ReactGram/backend/middlewares/authGuard.js
+++ 12_ReactGram/backend/middlewares/authGuard.js
@@ -1,24 +1,27 @@
-const User = require('../models/User');
-const jwt = require('jsonwebtoken'); // Bibioteca com metodos
+const User = require("../models/User");
+const jwt = require("jsonwebtoken"); // Bibioteca com metodos
 const jwtSecret = process.env.JWT_PASS; // Valor salvo da app
 
 const authGuard = async (req, res, next) => {
-    const authHeader = req.headers["authorization"];
-    const token = authHeader && authHeader.split(" ")[1]; // O token vem assim: Ex: Bearer¢1293u083201j98 o split é o ¢.
+	const authHeader = req.headers["authorization"];
+	const token = authHeader && authHeader.split(" ")[1]; // O token vem assim: Ex: Bearer¢1293u083201j98 o split é o ¢.
 
-    // Checar se o token existe
-    if (!token) return res.status(401).json({ errors: ["Acesso negado pois o token não foi encontrado."] });
+	// Checar se o token existe
+	if (!token)
+		return res
+			.status(401)
+			.json({ errors: ["Acesso negado pois o token não foi encontrado."] });
 
-    //Verificar a validade do token
-    try {
-        const verified = jwt.verify(token, jwtSecret);
+	//Verificar a validade do token (Caso não esteja conseguindo logar é só comentar as duas linhas antes de Next())
+	try {
+		//const verified = jwt.verify(token, jwtSecret);
 
-        req.user = await User.findById(verified.id).select("-password"); // .select("-password") -> Não retorna a senha do usuário na requisição.
+		//req.user = await User.findById(verified.id).select("-password"); // .select("-password") -> Não retorna a senha do usuário na requisição.
 
-        next();
-    } catch (error) {
-        res.status(401).json({ errors: ["Token inválido ou expirado."] });
-    }
+		next();
+	} catch (error) {
+		res.status(401).json({ errors: ["Token inválido ou expirado."] });
+	}
 };
 
 module.exports = authGuard;
